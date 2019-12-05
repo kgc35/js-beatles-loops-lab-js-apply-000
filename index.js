bc01fae8d148537d6b77292b413a4a5450d0416c
Kent Chan <kntchn@gmail.com>
2019-12-05 02:25:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(num) {
   let i = 15-num;
   let emptArray = [];
-  
+
   while(i <= 15 && num < 15) {
     emptArray.push("I love the Beatles!");
     i++;
@@ -30,6 +30,6 @@ function iLoveTheBeatles(num) {
   if(num > 15) {
     emptArray.push("I love the Beatles!")
   }
-  
+
   return emptArray;
 }
